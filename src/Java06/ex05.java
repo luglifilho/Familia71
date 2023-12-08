a783efb1a2be7e55e9ac0dfb94b31bc62221b41b
luglifilho <luglifilho@hotmail.com>
2023-12-08 13:47:58 -0300

    contJavva06exs

--- src/Java06/ex05.java
+++ src/Java06/ex05.java
@@ -1,9 +1,36 @@
+/* Escreva um algoritmo em Java, que leia números inteiros via 
+ * teclado, até que o número zero seja digitado. 
+ * Ao final, mostre na tela a soma de todos os números digitados,
+ *  que sejam positivos. Veja o exemplo abaixo:
+*/ 
+
+
 package Java06;
+
+import java.util.Scanner;
 
 public class ex05 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner (System.in);
+		
+		int num , soma = 0;
+		
+		do {
+			System.out.println("Digite um número: ");
+			num = scanner.nextInt();
+			
+			if(num > 0) {
+				soma += num;
+			}
+			
+			
+		}while(num != 0);
+		
+		System.out.println("A soma dos número positivos é: " +soma);
+		
+		scanner.close();
+		
 
 	}
 
--- src/Java06/ex06.java
+++ src/Java06/ex06.java
@@ -1,9 +1,41 @@
+/* 
+ * Escreva um algoritmo em Java, que leia números inteiros via teclado,
+ *  até que o número zero seja digitado.
+ *   Ao final, mostre na tela a média de todos os números digitados,
+ *  que sejam múltiplos de 3. 
+ */
+
 package Java06;
+
+import java.util.Scanner;
 
 public class ex06 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		int num;
+		float soma=0;
+		float count=0;
+		float avarage;
+		
+		do {
+			System.out.println("Digite um número: ");
+			num = scanner.nextInt();
+			
+			if( num % 3 == 0 && num != 0) {
+				soma += num;
+				count++;				
+			}
+			
+		
+			
+		}while(num != 0);
+		
+		avarage = soma/count;
+	
+		System.out.printf("A média de todos os números múltiplos de 3 é: %.1f%n" ,avarage );
+		
+		scanner.close();
 
 	}
 
